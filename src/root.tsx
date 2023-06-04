f4473346298d4e489b08054955fe6d1956926516
Jumpei Mori <nikutto.kom@gmail.com>
2023-06-04 15:10:14 +0900

    Divide header

--- src/root.tsx
+++ src/root.tsx
@@ -14,13 +14,11 @@ import {
   Title,
 } from "solid-start";
 import "./root.css";
+import Header from "./hearder";
 
 export default function Root() {
   const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+
   return (
     <Html lang="en">
       <Head>
@@ -31,16 +29,7 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                  <A href="/">Home</A>
-                </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
-                </li>
-              </ul>
-            </nav>
+            <Header/>
             <Routes>
               <FileRoutes />
             </Routes>
